d3caf54e808bbb676971e59870f22eef71e7e185
danielforkner <daniel.forkner@gmail.com>
2022-01-31 21:15:58 -0500

    added color slider

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@
 
 populateGrid(10);
 
+// nav buttons
+let colorBtn = document.querySelector('.colorBtn');
+
+colorBtn.addEventListener("click", function() {
+    document.querySelector('#colorSlider').classList.toggle('disabled');
+})
+
 
 // size is the number of rows & columns e.g. '25' == 25x25
 function populateGrid(size) {
@@ -16,6 +23,43 @@ function populateGrid(size) {
     for (let i = 0; i < count; i++) {
         pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        // change color when mouse over
+        pixel.addEventListener('mouseenter', function(event) {
+            let pickedColor = getComputedStyle(document.documentElement).getPropertyValue('--selectedColor');
+            event.target.style.backgroundColor = pickedColor;
+        })
         grid.append(pixel);
     }
 }
+
+// color picker nav item
+let colorPicker = new iro.ColorPicker('#colorSlider', {
+    color: "rgb(255, 0, 0)",
+    borderWidth: 1,
+    borderColor: "#fff",
+    layoutDirection: 'horizontal',
+    layout: [
+        {
+          component: iro.ui.Slider,
+          options: {
+            sliderType: 'hue',
+          }
+        },
+        {
+           component: iro.ui.Slider,
+           options: {
+            sliderType: 'value',
+            }
+        },
+        {
+           component: iro.ui.Slider,
+           options: {
+            sliderType: 'saturation',
+            }
+        },
+      ]
+});
+
+colorPicker.on('color:change', function(color) {
+    document.documentElement.style.setProperty('--selectedColor', color.hexString);
+})
